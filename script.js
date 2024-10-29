8ba12006c7d605225b7985426b9967ee25533d2c
Yassir Zbida <zbidayassir10@gmail.com>
2024-10-29 21:24:13 +0100

    Add task Function

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-// Create task 
 const addTaskModal = document.getElementById('addTaskModal');
 const addTask = document.getElementById('addTask');
 const cancel = document.getElementById('cancel');
+const taskTitle = document.getElementById('taskTitle');
+const taskDescription = document.getElementById('taskDescription');
+const taskStatus = document.getElementById('taskStatus');
+const taskDate = document.getElementById('taskDate');
+const taskPriority = document.getElementById('taskPriority');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const todoDiv = document.getElementById('todoContainer');
+let tasks = [];
 
 // Popup Open and Close
-addTask.addEventListener('click',function(){
+addTask.addEventListener('click', function() {
     addTaskModal.classList.remove('hidden');
 });
-cancel.addEventListener('click', function(){
+
+cancel.addEventListener('click', function() {
     addTaskModal.classList.add('hidden');
 });
 
-//
+// Add Task
+addTaskBtn.addEventListener('click', function() {
+    const newTask = {
+        title: taskTitle.value,
+        description: taskDescription.value,
+        status: taskStatus.value,
+        date: taskDate.value,
+        priority: taskPriority.value
+    };
 
-// Affiche Task
-// save localstorage
-// clear inputs
-// read
-// count total
-// delete
-// search 
-// clean data
+    tasks.push(newTask);
+    displayTasks();
+    clearInputs(); 
+    addTaskModal.classList.add('hidden'); 
+});
+
+// clear inputs of forms 
+function clearInputs() {
+    taskTitle.value = '';
+    taskDescription.value = '';
+    taskStatus.value = '';
+    taskDate.value = '';
+    taskPriority.value = '';
+}
+
