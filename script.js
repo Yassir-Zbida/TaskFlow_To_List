9809dbc4a0dc8a52888f7b6c789daca102c318c2
Yassir Zbida <zbidayassir10@gmail.com>
2024-10-29 19:48:00 +0100

    Add Popup close and open function

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 // Create task 
+const addTaskModal = document.getElementById('addTaskModal');
+const addTask = document.getElementById('addTask');
+const cancel = document.getElementById('cancel');
+
+// Popup Open and Close
+addTask.addEventListener('click',function(){
+    addTaskModal.classList.remove('hidden');
+});
+cancel.addEventListener('click', function(){
+    addTaskModal.classList.add('hidden');
+});
+
+//
+
 // Affiche Task
 // save localstorage
 // clear inputs
