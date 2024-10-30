449c7e2fc0e245f829c8f4a317da966c7501726f
Yassir Zbida <zbidayassir10@gmail.com>
2024-10-30 22:51:06 +0100

    add edit function and delete function

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ const addTaskBtn = document.getElementById('addTaskBtn');
 const totalTaskTodo = document.getElementById('todoTotal');
 const totalTaskDoing = document.getElementById('doingTotal');
 const totalTaskDone = document.getElementById('doneTotal');
+const editTask = document.getElementById('editTaskModal');
+const editBtn = document.getElementById('taskedit');
+const cancelEditTask = document.getElementById('cancelEditTask');
+
+const editTaskTitle = document.getElementById('editTaskTitle');
+const editTaskDescription = document.getElementById('editTaskDescription');
+const editTaskStatus = document.getElementById('editTaskStatus');
+const editTaskDate = document.getElementById('editTaskDate');
+const editTaskPriority = document.getElementById('editTaskPriority');
+const editTaskBtn = document.getElementById('editTaskBtn');
+
+
 let tasks = [];
 
 // Popup Open and Close
@@ -20,6 +32,62 @@ addTask.addEventListener('click', function() {
 cancel.addEventListener('click', function() {
     addTaskModal.classList.add('hidden');
 });
+// Add Task
+addTaskBtn.addEventListener('click', function() {
+    const newTask = {
+        title: taskTitle.value,
+        description: taskDescription.value,
+        status: taskStatus.value,
+        date: taskDate.value,
+        priority: taskPriority.value
+    };
+
+    tasks.push(newTask);
+
+    clearInputs(); 
+    updateTaskCounters();
+    
+    addTaskModal.classList.add('hidden');
+    displayTask();
+});
+function hideModal(){
+    editTask.classList.add('hidden');
+
+    
+}
+
+// Edit Popup 
+function showEditModal(index){
+    editTask.classList.remove('hidden');
+    console.log(tasks[index].title);
+    editTaskTitle.value = tasks[index].title ;
+    editTaskDescription.value = tasks[index].description ;
+    editTaskStatus.value = tasks[index].status ;
+    editTaskDate.value = tasks[index].date ;
+    editTaskPriority.value = tasks[index].priority;
+    editTaskBtn.addEventListener('click',function(){
+        const editTask = {
+            title: editTaskTitle.value,
+            description: editTaskDescription.value,
+            status: editTaskStatus.value,
+            date: editTaskDate.value,
+            priority: editTaskPriority.value
+        };
+        tasks[index]= editTask;
+        console.log(tasks[index]);
+        displayTask();
+        hideModal();
+        updateTaskCounters();
+    })
+    
+}
+
+
+
+// close PopUp 
+function closeEditModal(){
+    editTask.classList.add('hidden');
+}
 
 // Clear inputs of forms 
 function clearInputs() {
@@ -47,23 +115,6 @@ function updateTaskCounters() {
     totalTaskDone.textContent = doneCount;
 }
 
-// Add Task
-addTaskBtn.addEventListener('click', function() {
-    const newTask = {
-        title: taskTitle.value,
-        description: taskDescription.value,
-        status: taskStatus.value,
-        date: taskDate.value,
-        priority: taskPriority.value
-    };
-
-    tasks.push(newTask);
-    console.log(tasks.length);
-    clearInputs(); 
-    updateTaskCounters();
-    addTaskModal.classList.add('hidden');
-    displayTask();
-});
 
 // Display Task 
 function displayTask() {
@@ -77,19 +128,26 @@ function displayTask() {
     for (let i = 0; i < tasks.length; i++) {
         const status = tasks[i].status.toLowerCase();
         if (status === 'todo') {
-            createHtml(todo, tasks[i]); // Pass the current task to createHtml
+            createHtml(todo, tasks[i],i); 
         } else if (status === 'doing') {
-            createHtml(doing, tasks[i]);
+            createHtml(doing, tasks[i], i);
         } else if (status === 'done') {
-            createHtml(done, tasks[i]);
+            createHtml(done, tasks[i],i);
         }
     }
+    console.log(tasks)
 }
-
+// delete task 
+function deleteTask(index){
+    console.log(index);
+    
+    tasks.splice(index , 1)
+    displayTask();
+    updateTaskCounters();
+}
 // Create Task card 
-function createHtml(placeholder, task) { 
+function createHtml(placeholder, task, index) { 
     const div = document.createElement('div');
-
     let borderColorClass;
     if (task.priority === 'P0') {
         borderColorClass = 'border-red-500';
@@ -105,10 +163,12 @@ function createHtml(placeholder, task) {
                             <p class="text-sm text-gray-500">Due: ${task.date}</p>
                         </div>
                         <div class="flex items-center space-x-2">
-                            <button class="text-red-500 hover:text-red-700">Delete</button>
-                            <button class="text-yellow-500 hover:text-yellow-700">Edit</button>
+                            <button onclick= "deleteTask(${index})" class="text-red-500 hover:text-red-700 " id="deleteTask" >Delete</button>
+                            <button onclick='showEditModal(${index})' class="text-yellow-500 hover:text-yellow-700" id="taskedit" >Edit</button>
                         </div>
                     </div>`;
+                 
     placeholder.appendChild(div);
+    
 }
 
