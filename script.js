80f3e2ac61f18e1eb8d00da4113c0e2c43d8fa59
Yassir Zbida <zbidayassir10@gmail.com>
2024-11-03 18:09:02 +0100

    Add automatic filter function

--- script.js
+++ script.js
@@ -1,215 +1,220 @@
-const addTaskModal = document.getElementById('addTaskModal');
-const addTask = document.getElementById('addTask');
-const cancel = document.getElementById('cancel');
-const taskTitle = document.getElementById('taskTitle');
-const taskDescription = document.getElementById('taskDescription');
-const taskStatus = document.getElementById('taskStatus');
-const taskDate = document.getElementById('taskDate');
-const taskPriority = document.getElementById('taskPriority');
-const addTaskBtn = document.getElementById('addTaskBtn');
-const totalTaskTodo = document.getElementById('todoTotal');
-const totalTaskDoing = document.getElementById('doingTotal');
-const totalTaskDone = document.getElementById('doneTotal');
-const editTask = document.getElementById('editTaskModal');
-const editBtn = document.getElementById('taskedit');
-const cancelEditTask = document.getElementById('cancelEditTask');
-const editTaskTitle = document.getElementById('editTaskTitle');
-const editTaskDescription = document.getElementById('editTaskDescription');
-const editTaskStatus = document.getElementById('editTaskStatus');
-const editTaskDate = document.getElementById('editTaskDate');
-const editTaskPriority = document.getElementById('editTaskPriority');
-const editTaskBtn = document.getElementById('editTaskBtn');
-const searchInput = document.getElementById('searchInput');
+const addTaskModal = document.getElementById("addTaskModal");
+const addTask = document.getElementById("addTask");
+const cancel = document.getElementById("cancel");
+const taskTitle = document.getElementById("taskTitle");
+const taskDescription = document.getElementById("taskDescription");
+const taskStatus = document.getElementById("taskStatus");
+const taskDate = document.getElementById("taskDate");
+const taskPriority = document.getElementById("taskPriority");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const totalTaskTodo = document.getElementById("todoTotal");
+const totalTaskDoing = document.getElementById("doingTotal");
+const totalTaskDone = document.getElementById("doneTotal");
+const editTask = document.getElementById("editTaskModal");
+const editBtn = document.getElementById("taskedit");
+const cancelEditTask = document.getElementById("cancelEditTask");
+const editTaskTitle = document.getElementById("editTaskTitle");
+const editTaskDescription = document.getElementById("editTaskDescription");
+const editTaskStatus = document.getElementById("editTaskStatus");
+const editTaskDate = document.getElementById("editTaskDate");
+const editTaskPriority = document.getElementById("editTaskPriority");
+const editTaskBtn = document.getElementById("editTaskBtn");
+const searchInput = document.getElementById("searchInput");
+
 let tasks = [];
 
 // Popup Open and Close
-addTask.addEventListener('click', function() {
-    addTaskModal.classList.remove('hidden');
+addTask.addEventListener("click", function () {
+  addTaskModal.classList.remove("hidden");
 });
 
-cancel.addEventListener('click', function() {
-    addTaskModal.classList.add('hidden');
+cancel.addEventListener("click", function () {
+  addTaskModal.classList.add("hidden");
 });
- // Add Task Form Validation
+// Add Task Form Validation
 function validateTaskForm() {
-    let errorMessage = '';
+  let errorMessage = "";
 
-    if (!taskTitle.value.trim()) {
-        errorMessage += 'Task title is required.\n';
-    }
+  if (!taskTitle.value.trim()) {
+    errorMessage += "Task title is required.\n";
+  }
 
-    if (!taskDescription.value.trim()) {
-        errorMessage += 'Task description is required.\n';
-    }
+  if (!taskDescription.value.trim()) {
+    errorMessage += "Task description is required.\n";
+  }
 
-    const dueDate = new Date(taskDate.value);
-    const today = new Date();
-    // if (!taskDate.value) {
-    //     errorMessage += 'Due date is required.\n';
-    // } else if (dueDate < today.setHours(0, 0, 0, 0)) {
-    //     errorMessage += 'Due date must be today or a future date.\n';
-    // }
+  const dueDate = new Date(taskDate.value);
+  const today = new Date();
+  // if (!taskDate.value) {
+  //     errorMessage += 'Due date is required.\n';
+  // } else if (dueDate < today.setHours(0, 0, 0, 0)) {
+  //     errorMessage += 'Due date must be today or a future date.\n';
+  // }
 
-    if (!taskPriority.value) {
-        errorMessage += 'Please select a priority level.\n';
-    }
+  if (!taskPriority.value) {
+    errorMessage += "Please select a priority level.\n";
+  }
 
-    if (!taskStatus.value) {
-        errorMessage += 'Please select a task status.\n';
-    }
+  if (!taskStatus.value) {
+    errorMessage += "Please select a task status.\n";
+  }
 
-    if (errorMessage) {
-        alert(errorMessage);
-        return false;
-    }
-    return true;
+  if (errorMessage) {
+    alert(errorMessage);
+    return false;
+  }
+  return true;
 }
-
 
 // Add Task
-addTaskBtn.addEventListener('click', function() {
-    if (!validateTaskForm()) {
-        return; 
-    }
-    const newTask = {
-        title: taskTitle.value,
-        description: taskDescription.value,
-        status: taskStatus.value,
-        date: taskDate.value,
-        priority: taskPriority.value
-    };
+addTaskBtn.addEventListener("click", function () {
+  if (!validateTaskForm()) {
+    return;
+  }
+  const newTask = {
+    title: taskTitle.value,
+    description: taskDescription.value,
+    status: taskStatus.value,
+    date: taskDate.value,
+    priority: taskPriority.value,
+  };
 
-    tasks.push(newTask);
-
-    clearInputs(); 
-    updateTaskCounters();
-    
-    addTaskModal.classList.add('hidden');
-    displayTask();
+  tasks.push(newTask);
+  clearInputs();
+  updateTaskCounters();
+  addTaskModal.classList.add("hidden");
+  sortTasks();
+  displayTask();
 });
 
-function hideModal(){
-    editTask.classList.add('hidden');  
+function hideModal() {
+  editTask.classList.add("hidden");
 }
 
-// Edit Popup 
+// Edit Popup
 function showEditModal(index) {
-    editTask.classList.remove('hidden');
-    editTaskTitle.value = tasks[index].title;
-    editTaskDescription.value = tasks[index].description;
-    editTaskStatus.value = tasks[index].status;
-    editTaskDate.value = tasks[index].date;
-    editTaskPriority.value = tasks[index].priority;
-    editTaskBtn.onclick = function() {
-        editTaskFunction(index);
-    };
+  editTask.classList.remove("hidden");
+  editTaskTitle.value = tasks[index].title;
+  editTaskDescription.value = tasks[index].description;
+  editTaskStatus.value = tasks[index].status;
+  editTaskDate.value = tasks[index].date;
+  editTaskPriority.value = tasks[index].priority;
+  editTaskBtn.onclick = function () {
+    editTaskFunction(index);
+  };
 }
-// Edit task 
+// Edit task
 function editTaskFunction(index) {
-    // if (!validateTaskForm()) {
-    //     return; 
-    // }
-    tasks[index] = {
-        title: editTaskTitle.value,
-        description: editTaskDescription.value,
-        status: editTaskStatus.value,
-        date: editTaskDate.value,
-        priority: editTaskPriority.value
-    };
-    displayTask();
-    hideModal();
-    updateTaskCounters();
+  // if (!validateTaskForm()) {
+  //     return;
+  // }
+  tasks[index] = {
+    title: editTaskTitle.value,
+    description: editTaskDescription.value,
+    status: editTaskStatus.value,
+    date: editTaskDate.value,
+    priority: editTaskPriority.value,
+  };
+  sortTasks();
+  displayTask();
+  hideModal();
+  updateTaskCounters();
 }
 
-// close PopUp 
-function closeEditModal(){
-    editTask.classList.add('hidden');
+// close PopUp
+function closeEditModal() {
+  editTask.classList.add("hidden");
 }
 
-// Clear inputs of forms 
+// Clear inputs of forms
 function clearInputs() {
-    taskTitle.value = '';
-    taskDescription.value = '';
-    taskStatus.value = '';
-    taskDate.value = '';
-    taskPriority.value = '';
+  taskTitle.value = "";
+  taskDescription.value = "";
+  taskStatus.value = "";
+  taskDate.value = "";
+  taskPriority.value = "";
 }
 
-// Update Total Counter Span 
+// Update Total Counter Span
 function updateTaskCounters() {
-    let todoCount = 0;
-    let doingCount = 0;
-    let doneCount = 0;
+  let todoCount = 0;
+  let doingCount = 0;
+  let doneCount = 0;
 
-    tasks.forEach(task => {
-        if (task.status === 'todo') todoCount++;
-        else if (task.status === 'doing') doingCount++;
-        else if (task.status === 'done') doneCount++;
-    });
+  tasks.forEach((task) => {
+    if (task.status === "todo") todoCount++;
+    else if (task.status === "doing") doingCount++;
+    else if (task.status === "done") doneCount++;
+  });
 
-    totalTaskTodo.textContent = todoCount;
-    totalTaskDoing.textContent = doingCount;
-    totalTaskDone.textContent = doneCount;
+  totalTaskTodo.textContent = todoCount;
+  totalTaskDoing.textContent = doingCount;
+  totalTaskDone.textContent = doneCount;
 }
 
-
-// Display Task 
+// Display Task
 function displayTask() {
-    const todo = document.getElementById('todoContainer');
-    const doing = document.getElementById('doingContainer');
-    const done = document.getElementById('doneContainer');
-    todo.innerHTML = "";
-    doing.innerHTML = "";
-    done.innerHTML = "";
+  const todo = document.getElementById("todoContainer");
+  const doing = document.getElementById("doingContainer");
+  const done = document.getElementById("doneContainer");
+  todo.innerHTML = "";
+  doing.innerHTML = "";
+  done.innerHTML = "";
 
-    for (let i = 0; i < tasks.length; i++) {
-        const status = tasks[i].status.toLowerCase();
-        if (status === 'todo') {
-            createHtml(todo, tasks[i],i); 
-        } else if (status === 'doing') {
-            createHtml(doing, tasks[i], i);
-        } else if (status === 'done') {
-            createHtml(done, tasks[i],i);
-        }
+  for (let i = 0; i < tasks.length; i++) {
+    const status = tasks[i].status.toLowerCase();
+    if (status === "todo") {
+      createHtml(todo, tasks[i], i);
+    } else if (status === "doing") {
+      createHtml(doing, tasks[i], i);
+    } else if (status === "done") {
+      createHtml(done, tasks[i], i);
     }
-    
+  }
 }
 
-// delete task 
+// delete task
 function deleteTask(index) {
-    const taskContainers = document.querySelectorAll('.taskContainer');
-    const taskToRemove = taskContainers[index];
-    taskToRemove.classList.add('transition-opacity', 'duration-500', 'opacity-0');
-    setTimeout(() => {
-        tasks.splice(index, 1);
-        displayTask();
-        updateTaskCounters();
-    }, 500);
+  const taskContainers = document.querySelectorAll(".taskContainer");
+  const taskToRemove = taskContainers[index];
+  taskToRemove.classList.add("transition-opacity", "duration-500", "opacity-0");
+  setTimeout(() => {
+    tasks.splice(index, 1);
+    displayTask();
+    updateTaskCounters();
+  }, 500);
 }
 
-// Create Task card 
-function createHtml(placeholder, task, index) { 
-    const div = document.createElement('div');
-    let borderColorClass;
-    let bgColor;
+// Create Task card
+function createHtml(placeholder, task, index) {
+  const div = document.createElement("div");
+  let borderColorClass;
+  let bgColor;
 
-    if (task.priority === 'P0') {
-        borderColorClass = 'border-red-500';
-        bgColor = 'bg-red-500/[.08]';
-    } else if (task.priority === 'P1') {
-        borderColorClass = 'border-yellow-400';
-        bgColor = 'bg-yellow-500/[.08]';
-    } else if (task.priority === 'P2') {
-        borderColorClass = 'border-green-500';
-        bgColor = 'bg-green-500/[.08]';
-    }
+  if (task.priority === "1") {
+    borderColorClass = "border-red-500";
+    bgColor = "bg-red-500/[.08]";
+  } else if (task.priority === "2") {
+    borderColorClass = "border-yellow-400";
+    bgColor = "bg-yellow-500/[.08]";
+  } else if (task.priority === "3") {
+    borderColorClass = "border-green-500";
+    bgColor = "bg-green-500/[.08]";
+  }
 
-    div.classList.add(
-        'taskContainer', 'p-4', 'border-l-4', borderColorClass, bgColor,
-        'rounded-lg', 'flex', 'flex-col', 'space-y-2'
-    );
+  div.classList.add(
+    "taskContainer",
+    "p-4",
+    "border-l-4",
+    borderColorClass,
+    bgColor,
+    "rounded-lg",
+    "flex",
+    "flex-col",
+    "space-y-2"
+  );
 
-    div.innerHTML = `
+  div.innerHTML = `
         <div class="flex flex-col space-y-1">
             <h3 class="font-semibold text-lg text-gray-900 overflow-hidden whitespace-normal break-words">
                 ${task.title}
@@ -229,43 +234,58 @@ function createHtml(placeholder, task, index) {
         </div>
     `;
 
-    placeholder.appendChild(div);
+  placeholder.appendChild(div);
 }
 
-searchInput.addEventListener('input', searchTask);
+searchInput.addEventListener("input", searchTask);
 
-// Task search function 
+// Task search function
 function searchTask() {
-    const filteredTasks = [];
-    const keyword = searchInput.value.toUpperCase();
-    
-    for (let i = 0; i < tasks.length; i++) {
-        const taskTitle = tasks[i].title.toUpperCase();
-        if (taskTitle.includes(keyword)) { 
-            filteredTasks.push(tasks[i]);
-        }
+  const filteredTasks = [];
+  const keyword = searchInput.value.toUpperCase();
+
+  for (let i = 0; i < tasks.length; i++) {
+    const taskTitle = tasks[i].title.toUpperCase();
+    if (taskTitle.includes(keyword)) {
+      filteredTasks.push(tasks[i]);
     }
-    displayFilteredTasks(filteredTasks);
+  }
+  displayFilteredTasks(filteredTasks);
 }
 
 // display filtered tasks
 function displayFilteredTasks(filteredTasks) {
-    const todo = document.getElementById('todoContainer');
-    const doing = document.getElementById('doingContainer');
-    const done = document.getElementById('doneContainer');
+  const todo = document.getElementById("todoContainer");
+  const doing = document.getElementById("doingContainer");
+  const done = document.getElementById("doneContainer");
 
-    todo.innerHTML = "";
-    doing.innerHTML = "";
-    done.innerHTML = "";
+  todo.innerHTML = "";
+  doing.innerHTML = "";
+  done.innerHTML = "";
 
-    for (let i = 0; i < filteredTasks.length; i++) {
-        const status = filteredTasks[i].status.toLowerCase();
-        if (status === 'todo') {
-            createHtml(todo, filteredTasks[i], i);
-        } else if (status === 'doing') {
-            createHtml(doing, filteredTasks[i], i);
-        } else if (status === 'done') {
-            createHtml(done, filteredTasks[i], i);
-        }
+  for (let i = 0; i < filteredTasks.length; i++) {
+    const status = filteredTasks[i].status.toLowerCase();
+    if (status === "todo") {
+      createHtml(todo, filteredTasks[i], i);
+    } else if (status === "doing") {
+      createHtml(doing, filteredTasks[i], i);
+    } else if (status === "done") {
+      createHtml(done, filteredTasks[i], i);
     }
+  }
+}
+
+// Sort All Tasks
+
+function sortTasks() {
+  let tmp;
+  for (let i = 0; i < tasks.length; i++) {
+    for (let j = 0; j < tasks.length - 1; j++) {
+      if (tasks[j].priority > tasks[j + 1].priority) {
+        tmp = tasks[j];
+        tasks[j] = tasks[j + 1];
+        tasks[j + 1] = tmp;
+      }
+    }
+  }
 }
